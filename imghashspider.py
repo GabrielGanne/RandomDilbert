6a2c79874f31821a6f2fd850b22c172c32a5dc9d
Gabriel Ganne <gabriel.ganne@qosmos.com>
2015-05-25 12:23:03 +0200

    use datetime for dates

--- imghashspider.py
+++ imghashspider.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+
 from __future__ import print_function
 
 import urllib2
 import re
+import datetime
+from dateutil.rrule import rrule, DAILY
 
-from settings import URL_FILE, DILBERT_URL_PATTERN
+from settings import URL_FILE, DILBERT_URL_PATTERN, DILBERT_FIRST_COMMIC_DATE
 
 img_pattern = re.compile('<meta property="og:image" content="http://assets.amuniversal.com/([0-9a-fA-F]*)"/>')
 
@@ -11,18 +15,18 @@ img_pattern = re.compile('<meta property="og:image" content="http://assets.amuni
 def urlcrawler():
 
     with open(URL_FILE, 'w') as url_file:
+        date_start = datetime.datetime.strptime(DILBERT_FIRST_COMMIC_DATE, "%Y-%m-%d")
+        date_end = datetime.date.today()
 
-        for year in range(2009, 2015):
-            for month in range(1, 13):
-                for day in range(1, 31):
-                    try:
-                        url_to_dilbert_page = DILBERT_URL_PATTERN % (year, month, day)
-                        page_contents = urllib2.urlopen(url_to_dilbert_page).read()
-                        image_hash = img_pattern.search(page_contents).group(1)
-                        print(image_hash, file=url_file)
-                        print('wrote hash for {0}-{1}-{2}'.format(year, month, day))
-                    except Exception as e:
-                        print('error', e)
+        for dt in rrule(DAILY, dtstart=date_start, until=date_end):
+            try:
+                url_to_dilbert_page = DILBERT_URL_PATTERN % dt.strftime("%Y-%m-%d")
+                page_contents = urllib2.urlopen(url_to_dilbert_page).read()
+                image_hash = img_pattern.search(page_contents).group(1)
+                print(image_hash, file=url_file)
+                print('wrote hash for {0} : {1}'.format(dt.strftime("%Y-%m-%d"), image_hash))
+            except Exception as e:
+                print('error', e)
 
 if __name__ == "__main__":
     urlcrawler()
--- settings.py
+++ settings.py
@@ -2,5 +2,8 @@ import os
 
 URL_FILE = os.path.join(os.path.dirname(__file__), 'img_urls.txt')
 
-DILBERT_URL_PATTERN = "http://www.dilbert.com/strip/%s-%s-%s/"
+# http://www.dilbert.com/strip/%Y-%m-%d
+DILBERT_URL_PATTERN = "http://www.dilbert.com/strip/%s/"
+DILBERT_FIRST_COMMIC_DATE = "1989-04-16"
+
 DILBERT_IMG_URL = "http://assets.amuniversal.com/"
