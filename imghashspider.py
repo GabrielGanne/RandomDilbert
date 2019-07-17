68d45c3b93b984061d4b2c1acea7707f267e1a7f
Gabriel Ganne <gabriel.ganne@mindmaze.ch>
2019-07-17 16:08:40 +0200

    update crawler to python3

--- imghashspider.py
+++ imghashspider.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from __future__ import print_function
 
-import urllib2
+import urllib3
 import re
 import datetime
 from dateutil.rrule import rrule, DAILY
@@ -11,6 +11,7 @@ from settings import URL_FILE, DILBERT_URL_PATTERN, DILBERT_FIRST_COMMIC_DATE
 
 img_pattern = re.compile('<meta property="og:image" content="http://assets.amuniversal.com/([0-9a-fA-F]*)"/>')
 
+http = urllib3.PoolManager()
 
 def urlcrawler():
 
@@ -21,7 +22,7 @@ def urlcrawler():
         for dt in rrule(DAILY, dtstart=date_start, until=date_end):
             try:
                 url_to_dilbert_page = DILBERT_URL_PATTERN % dt.strftime("%Y-%m-%d")
-                page_contents = urllib2.urlopen(url_to_dilbert_page).read()
+                page_contents = http.request('GET', url_to_dilbert_page).data.decode("utf-8") 
                 image_hash = img_pattern.search(page_contents).group(1)
                 print(image_hash, file=url_file)
                 print('wrote hash for {0} : {1}'.format(dt.strftime("%Y-%m-%d"), image_hash))
